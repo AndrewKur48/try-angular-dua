2079ea0565790eedec21fc903430f8a4e6f7c822
AndrewKur48 <andrewk4820@gmail.com>
2020-04-22 10:38:16 +0700

    2 files modified

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-
-  constructor() { }
+  tempnam = "";
+  temppen = "";
+  constructor(private router :Router) { }
 
   ngOnInit() {
   }
-
+  sInput(nam,pen){
+    this.tempnam = nam;
+    this.temppen = pen;
+    this.router.navigate(['/list',this.tempnam,this.temppen]);
+  }
 }
